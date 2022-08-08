335fff44cf88dc2c378e133b76cc88e8c1b34165
ATOJ5 <dgc105@gmail.com>
2022-08-08 19:49:29 +0200

    main program

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let estanteria = document.getElementById("estanteria");
+let archivador = document.getElementById("archivador");
+let contact = document.getElementById("contact");
+
+//Now, we need to add an Event Listener to listen when the image gets mouse over.
+
+estanteria.addEventListener('mouseover', function(){
+   
+})
+estanteria.addEventListener('mouseout', function(){
+    
+  })
